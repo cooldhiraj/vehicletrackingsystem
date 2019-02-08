147e3e673dfe6abeff91d6fec2d9a49de391a924
dhiraj kahar <dhirajkahar7@gmail.com>
2019-02-08 15:08:11 +0530

    add req, res

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ mongoose.connect('mongodb://vehicleuser:#Dhiraj0731@ds227255.mlab.com:27255/vehi
     .then(() => console.log('Sucessfully Connected to Database'))
     .catch(err => console.error('Error ', err));
 
-app.get('/', () => {
+app.get('/', (req, res) => {
     res.send('Welcome To My Web Application');
     console.log('Welcome To My Console Application');
 });
