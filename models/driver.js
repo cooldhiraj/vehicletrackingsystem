46fcddecd89ddaca7c83733ce71d5ac9d8950b0c
dhiraj kahar <dhirajkahar7@gmail.com>
2019-02-08 14:49:38 +0530

    changes models

--- models/driver.js
+++ models/driver.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Joi = require('Joi');
+const Joi = require('joi');
 
 const Driver = mongoose.model('Driver', new mongoose.Schema({
     name: {
--- models/vehicle.js
+++ models/vehicle.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Joi = require('Joi');
+const Joi = require('joi');
 
 const Vehicle = mongoose.model('Vehicle', new mongoose.Schema({
     v_type: {
